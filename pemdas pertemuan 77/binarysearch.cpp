0ab791f15f04e86b7980b5e438665fc1f1a06a61
Dhika270 <andikasetiawan05ds@gmail.com>
2025-05-07 15:49:47 +0700

    commit 2

--- pemdas pertemuan 77/binarysearch.cpp
+++ pemdas pertemuan 77/binarysearch.cpp
@@ -6,5 +6,10 @@ int x;
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+    }
 }
