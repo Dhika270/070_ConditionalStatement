fa2168692ec6eaf6127f1e959f29f9993c85d1de
Dhika270 <andikasetiawan05ds@gmail.com>
2025-05-08 19:33:39 +0700

    commit 8

--- pemdas p8/algo.cpp
+++ pemdas p8/algo.cpp
@@ -95,4 +95,23 @@ class LinkedList
         delete current;
         return true;
     }
+
+    void traverse()
+    {
+        if (!listEmpty())
+        {
+            Node *currentNode = START;
+            cout << "\nData dalam list adalah:\n";
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << " ";
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+        else
+        {
+            cout << "\nList Kosong\n";
+        }
+    }
 }
